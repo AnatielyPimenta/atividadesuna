99224e1d37ccd4ffab73e900fa5c5a19a20e20e8
anatiely <anatielypimenta.6026@aluno.una.br>
2021-11-23 00:05:46 -0300

    pratica3

---
+++ pratica3/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+void main() {
+String nome = "Joana";
+runApp(
+Center(
+child: RichText(
+textDirection: TextDirection.ltr,
+text: TextSpan(
+text: "Olá, ",
+style: TextStyle(
+color: Colors.black,
+fontSize: 30,
+fontWeight: FontWeight.bold,
+backgroundColor: Colors.white,
+), //TextStyle.
+children: <TextSpan>[
+  TextSpan(
+text: '$nome',
+style: TextStyle(
+color: Colors.pink,
+decoration: TextDecoration.underline,
+decorationColor: Colors.red,
+decorationStyle: TextDecorationStyle.double,
+),
+),
+TextSpan(
+text: '! \nHoje é sexta-feira!',
+),
+],
+),
+),
+),
+);
+}
